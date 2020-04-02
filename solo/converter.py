226782df2ea58bb32339538e22c438b9daa262dd
envomp <envomp@ttu.ee>
2020-04-03 00:21:00 +0300

    solo

--- solo/converter.py
+++ solo/converter.py
@@ -7,11 +7,12 @@ def convert_dict_to_csv(dict, public):
 
     if public:
         csv_columns = ['id', 'lastTested', 'totalCommits', 'totalTestsRan', 'totalTestsPassed',
-                       'totalDiagnosticErrors', 'differentSlugs', 'differentCourses', 'commitsStyleOK']
+                       'totalDiagnosticErrors', 'differentSlugs', 'differentCourses', 'commitsStyleOK', 'averageGrade',
+                       'medianGrade']
     else:
         csv_columns = ['id', 'uniid', 'lastTested', 'totalCommits', 'totalTestsRan', 'totalTestsPassed',
-                       'totalDiagnosticErrors',
-                       'differentSlugs', 'differentCourses', 'commitsStyleOK']
+                       'totalDiagnosticErrors', 'differentSlugs', 'differentCourses', 'commitsStyleOK', 'averageGrade',
+                       'medianGrade']
 
     try:
         with open(csv_file, 'w') as csvfile:
@@ -32,53 +33,3 @@ if __name__ == '__main__':
         dict_data = json.load(f)
 
     convert_dict_to_csv(dict_data, True)
-
-
-
-class Root:
-
-    def __init__(self, left, right, val):
-        self.left = left
-        self.right = right
-        self.val = val
-
-
-def inOrder(root):
-    if root:
-        inOrder(root.left)
-        print(root.val)
-        inOrder(root.right)
-
-
-def preOrder(root):
-    if root:
-        print(root.val)
-        inOrder(root.left)
-        inOrder(root.right)
-
-
-def postOrder(root):
-    if root:
-        inOrder(root.left)
-        inOrder(root.right)
-        print(root.val)
-
-
-def compexity(n):
-    a = 0
-
-    for i in range(n): # n
-        BinaryTree.insert(n) # log(n)
-
-    for i in range(n):
-        for j in range(n):
-            a += 1
-
-
-if __name__ == '__main__':
-    root = Root(Root(Root(None, None, 1), Root(Root(None, None, 4), Root(None, None, 7), 6), 3), Root(None, Root(Root(None, None, 13), None, 14), 10), 8)
-    inOrder(root)
-    print()
-    preOrder(root)
-    print()
-    postOrder(root)
