b03d6b04cc3f9dfd9bda8c4681e18c6e395c6a71
envomp <envomp@ttu.ee>
2020-04-28 21:32:21 +0300

    solo

--- solo/converter.py
+++ solo/converter.py
@@ -7,17 +7,18 @@ def convert_dict_to_csv(dict):
 
     csv_columns = ['id', 'uniid', 'lastTested', 'totalCommits', 'totalTestsRan', 'totalTestsPassed',
                    'totalDiagnosticErrors', 'differentSlugs', 'differentCourses', 'commitsStyleOK', 'averageGrade',
-                   'medianGrade']  # Last one is the one being predicted
+                   'medianGrade']
 
     csv_columns_percent = ['totalDiagnosticErrors_per_totalCommits', 'totalTestsPassed_per_totalTestsRan',
-                           'commitsStyleOK_per_totalCommits', ]
+                           'commitsStyleOK_per_totalCommits']
 
     to_drop = ['id', 'uniid', 'lastTested', 'medianGrade']
     to_drop_extra_only = ['totalCommits', 'totalTestsRan', 'differentCourses', 'totalTestsPassed',
-                          'totalDiagnosticErrors', 'commitsStyleOK', 'commitsStyleOK_per_totalCommits']
+                          'totalDiagnosticErrors', 'commitsStyleOK', 'commitsStyleOK_per_totalCommits', 'differentSlugs']
 
     try:
 
+        # dict_bool = {}
         for i in range(3):
             with open(csv_file + ("_extra.csv" if i == 1 else ".csv" if not i else "_extra_only.csv"), 'w') as csvfile:
                 if i == 1:
@@ -25,19 +26,29 @@ def convert_dict_to_csv(dict):
 
                 if i == 2:
                     csv_columns = [x for x in csv_columns if x not in to_drop_extra_only]
+                    # csv_columns.append("isTalTechStudent")
 
                 csv_columns = [x for x in csv_columns if x not in to_drop]
 
                 writer = csv.DictWriter(csvfile, delimiter=',', lineterminator='\n', fieldnames=csv_columns)
 
-                csvfile.write(
-                    f"{len(dict)},{len(csv_columns) - 1}\n")  # -1 because the last field is the one being predicted
+                # if i == 2:
+                #     csvfile.write(f"{len(dict)},{len(csv_columns) - 1}\n")
+                #     # -2 because the last field is the one being predicted
+                # else:
+                csvfile.write(f"{len(dict)},{len(csv_columns) - 1}\n")
+                # -1 because the last field is the one being predicted
+
                 writer.writeheader()
 
-                for data in reversed(dict):
+                for j, data in enumerate(reversed(dict)):
                     if not i:
+                        # dict_bool[j] = 1 if "@" not in data["uniid"] else 0
+
                         for drop in to_drop:
                             del data[drop]
+
+                    # data["isTalTechStudent"] = dict_bool[j]
 
                     if i:
                         for extra in csv_columns_percent:
